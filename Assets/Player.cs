5514026395e164cad0a63b97f9b1aefefc74e2fc
Denys Kuksin <deniskuksin1@gmail.com>
2025-03-28 19:35:44 +0200

    Завершення кат-сцен. Додання ігрока

---
+++ Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    private NavMeshAgent agent;
+
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+
+
+        // Оп : ==, !=, >, <, <=, >=
+
+        // Умову : значення1 оп значення2
+
+        //if (Умова)
+        //{
+        //    Що буде відбуватись, якщо умова є правдивою
+        //}
+
+        var randomNumber = Random.Range(1, 100);
+
+        if (randomNumber > 50)
+        {
+            Debug.Log("Число більше 50");
+        }
+
+        Debug.Log(randomNumber);
+
+    }
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                agent.SetDestination(hit.point);
+
+        }
+
+
+
+
+        //Debug.Log(agent.velocity.magnitude);
+    }
+}
